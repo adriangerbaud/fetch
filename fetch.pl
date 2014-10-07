97ac885ef8ddf2f24416c3866d69739ef7519325
EC2 Default User <ec2-user@ip-172-31-35-1.ec2.internal>
2014-10-07 20:34:44 +0000

    Minor changes to make script more robust

--- fetch.pl
+++ fetch.pl
@@ -1,23 +1,29 @@
 #!/usr/bin/perl
 
+use Data::Dumper;
+use strict;
+use warnings;
+
 #Declare useful variables
 
-$UNIXenddate = `date +%s`;
+my $UNIXenddate = `date +%s`;
 chomp $UNIXenddate;
 
-$date = `date +%b-%d-%Y`;
+my $date = `date +%b-%d-%Y`;
 chomp $date;
 
-$datetime = `date "+%H:%M:%S %b-%d-%Y"`;
+my $datetime = `date "+%H:%M:%S %b-%d-%Y"`;
 chomp $datetime;
 
-$wd = `pwd`;
+my $wd = `pwd`;
 chomp $wd;
-$datapath = "$wd/data";
 
-$year = `date +%Y`;
+my$datapath = "$wd/data";
+
+my $year = `date +%Y`;
 chomp $year;
-$startdate= `date -d "$year-01-01 00:00:00" "+%s"`;
+
+my $startdate= `date -d "$year-01-01 00:00:00" "+%s"`;
 chomp $startdate;
 
 #make data folder if it does not exist
@@ -29,25 +35,29 @@ unless(-e $datapath)
 
 
 # Populate sites hash
+my %sites;
+open my $fh, '<', 'sites.csv' or die "Cannot open: $!";
+while(my $line = <$fh>)
+{
+    my @array = split/,/, $line;
+    $sites{$array[0]} = [$array[1] , "$datapath/$array[0]"];
+    
+}
 
-%sites = ( Lira => ["lmnd33", "$datapath/Lira"],
-	   Pabo => ["lmnd27", "$datapath/Pabo"],
-	   Lacor => ["lmnd34", "$datapath/Lacor"],
-	 );
+close $fh;
 
 #Download the data.
-foreach $sitecode (keys %sites)
+foreach my $sitecode (keys %sites)
 {
 	`curl "http://$sites{$sitecode}[0].egaug.es/cgi-bin/egauge-show\?d\&s=0\&c\&t=$startdate\&f=$UNIXenddate\&Z=:EAT" -o "$sites{$sitecode}[1]-$date.csv" -fs`;
-	$p= $? >> 8;
+	my  $p= $? >> 8;
 
 #If new data is availabe, erase the old data
-	if($p == 0 )
+	if( $p == 0 )
 	{	
 
-	$remove = `ls $datapath | grep '$sitecode' | grep -v "$date"`;
+	my $remove = `ls $datapath | grep '$sitecode' | grep -v "$date"`;
 	`rm -f $datapath/$remove`; #remove it if it is there
-#	print "$remove\n"
 	}
 }
 
@@ -59,9 +69,3 @@ open(FH, ">>", "$datapath/time.log") or die "Cannot open file";
 select FH;
 print "Script ran at $datetime\n";
 close FH;
-
-# Send file to windows server (192.168.3.16/Office)  mounted on ubuntu server (192.168.3.20)
-`mount -a`;
-`rm /mnt/Office/eGaugeData/*`;
-`cp $datapath/* /mnt/Office/eGaugeData`;
-
